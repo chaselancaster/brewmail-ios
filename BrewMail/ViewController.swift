cf94bb55b026693c42b0a73c9e5e7571d54b1abe
chaselancaster <chaseblancaster@gmail.com>
2020-05-28 14:48:58 -0700

    first commit

---
+++ BrewMail/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BrewMail
+//
+//  Created by Chase Lancaster on 5/28/20.
+//  Copyright © 2020 Chase Lancaster. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
