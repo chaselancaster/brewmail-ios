a317645cc4f01bf1235eed435985ade658ce13ac
chaselancaster <chaseblancaster@gmail.com>
2020-05-28 17:44:02 -0700

    Add segue from register button to RegisterViewController. Create RegisterViewController.swift

---
+++ BrewMail/RegisterViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RegisterViewController.swift
+//  BrewMail
+//
+//  Created by Chase Lancaster on 5/28/20.
+//  Copyright © 2020 Chase Lancaster. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class RegisterViewController: UIViewController {
+    
+    
+    
+}
