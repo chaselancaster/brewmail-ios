2ba0759bb0f3e619c0bc223e2908e84d30e203bd
chaselancaster <chaseblancaster@gmail.com>
2020-06-01 14:37:06 -0700

    Create SearchViewController

---
+++ BrewMail/SearchViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SearchViewController.swift
+//  BrewMail
+//
+//  Created by Chase Lancaster on 6/1/20.
+//  Copyright © 2020 Chase Lancaster. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UITableViewController {
+    
+    
+    
+}
