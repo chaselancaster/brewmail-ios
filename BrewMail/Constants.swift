0006e2d4d41ef03f273349126f6e0b9252911ab9
chaselancaster <chaseblancaster@gmail.com>
2020-06-01 14:09:18 -0700

    Create Constants.swift file for all constants

---
+++ BrewMail/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  BrewMail
+//
+//  Created by Chase Lancaster on 6/1/20.
+//  Copyright © 2020 Chase Lancaster. All rights reserved.
+//
+
+struct Constants {
+    static let registerSegue = "RegisterToSearch"
+}
