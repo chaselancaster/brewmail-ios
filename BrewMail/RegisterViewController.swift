a6ceef52a534864d7cce95acf18b2e2ab2cf024f
chaselancaster <chaseblancaster@gmail.com>
2020-06-01 13:46:01 -0700

    Add LoginViewController file and delete what is in the RegisterViewController

--- BrewMail/RegisterViewController.swift
+++ BrewMail/RegisterViewController.swift
@@ -11,19 +11,4 @@ import Firebase
 
 class RegisterViewController: UIViewController {
     
-//    @IBOutlet var emailTextfield: UITextField!
-//    @IBOutlet var passwordTextfield: UITextField!
-//    
-//    @IBAction func registerPressed(_ sender: Any) {
-//        
-//        if let email = emailTextfield.text, let password = passwordTextfield.text {
-//            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
-//                if let e = error {
-//                    print(e)
-//                } else {
-//                    //Navigate to ViewController
-//                }
-//            }
-//        }
-//    }
 }
