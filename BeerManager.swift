809641609785e0a9765a8cbacb6adf58aaced976
chaselancaster <chaseblancaster@gmail.com>
2020-06-02 09:57:40 -0700

    Create BeerManager.swift

---
+++ BeerManager.swift
@@ -0,0 +1,15 @@
+//
+//  BeerManager.swift
+//  BrewMail
+//
+//  Created by Chase Lancaster on 6/2/20.
+//  Copyright © 2020 Chase Lancaster. All rights reserved.
+//
+
+import Foundation
+
+struct BeerManager {
+    
+    
+    
+}
