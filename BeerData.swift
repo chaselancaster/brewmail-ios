b8476e0e7a5389e49f29c41854ac0c5208ea7881
chaselancaster <chaseblancaster@gmail.com>
2020-06-01 17:48:14 -0700

    Add BeerData.swift that acts as initial structure for Untappd API call.

---
+++ BeerData.swift
@@ -0,0 +1,33 @@
+//
+//  BeerData.swift
+//  BrewMail
+//
+//  Created by Chase Lancaster on 6/1/20.
+//  Copyright © 2020 Chase Lancaster. All rights reserved.
+//
+
+import Foundation
+
+struct Response: Codable {
+    
+    let count: Int
+    let items: [Item]
+    
+}
+
+struct Item: Codable {
+    
+    let beer: [Beer]
+    
+    
+}
+
+struct Beer: Codable {
+    
+    let beer_name: String
+    let beer_label: String
+    let beer_abv: String
+    let beer_ibu: String
+    let beer_description: String
+    
+}
