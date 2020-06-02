9d6c3601d96ce77097a7a0e0be6be6826acdfb2f
chaselancaster <chaseblancaster@gmail.com>
2020-06-02 10:16:24 -0700

    Set base URL for API calls in BeerManager.

--- BeerManager.swift
+++ BeerManager.swift
@@ -10,6 +10,9 @@ import Foundation
 
 struct BeerManager {
     
+    let baseURL = "https://api.untappd.com/v4/method_name?client_id=CLIENTID&client_secret=CLIENTSECRET"
     
+    let clientID = "890DD1105A2483401A50DEE63A4369641B1E9F2C"
+    let clientSecret = "ED374F4D5737AB079E99198D36F42B656E874F1F"
     
 }
