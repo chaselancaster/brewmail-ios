f18a9f36f3420c750d87a68857aa2ee36e123d9a
chaselancaster <chaseblancaster@gmail.com>
2020-06-19 14:25:46 -0700

    Add CellarViewController.

---
+++ BrewMail/Controller/CellarViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CellarViewController.swift
+//  BrewMail
+//
+//  Created by Chase Lancaster on 6/19/20.
+//  Copyright © 2020 Chase Lancaster. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CellarViewController: UITableViewController {
+    
+    
+    
+}
