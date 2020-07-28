ade85efe2e0c9aa07005b8f6be65c349dd13a27a
chaselancaster <chaseblancaster@gmail.com>
2020-07-28 11:39:57 -0700

    Move code out of viewDidLoad into a function called fundUserCellarBeer. Change cellForRowAt code to work with the dictionary format. Might change this later if I can find a way to use Codable with Firebase in order to fetch data.

--- BrewMail/Controller/CellarViewController.swift
+++ BrewMail/Controller/CellarViewController.swift
@@ -23,6 +23,18 @@ class CellarViewController: UITableViewController {
     
     override func viewDidLoad() {
         
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        // Clearing array so it doesn't duplicate every time a user checks cellar
+        cellarBeer = [[String:Any]]()
+        
+        findUserCellarBeer()
+        
+    }
+    
+    func findUserCellarBeer() {
+        
         // Finding current user's id
         guard let uid = Auth.auth().currentUser?.uid else { return }
         
@@ -35,6 +47,10 @@ class CellarViewController: UITableViewController {
                     self.cellarBeer.append(document.data())
                 }
                 print("cellarBeer ---->", self.cellarBeer)
+                // Reloading tableView to display cellar beer to user
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
             }
         }
         
@@ -44,33 +60,33 @@ class CellarViewController: UITableViewController {
 
 // MARK: - Table View Code
 
-//extension CellarViewController {
-//
-//    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-//        return 100
-//    }
-//
-//    override func numberOfSections(in tableView: UITableView) -> Int {
-//        return 1
-//    }
-//
-//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return cellarBeer.count
-//    }
-//
-//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.beerCell, for: indexPath) as! BeerTableViewCell
-//        let beer = cellarBeer[indexPath.row]
-//        cell.beerName.text = beer.beerName
-//        cell.breweryName.text = beer.breweryName
-//        cell.beerStyle.text = beer.beerStyle
-//        cell.beerLabel.sd_setImage(with: URL(string: beer.beerLabel), placeholderImage: UIImage(named: "placeholder.png"))
-////        cell.textLabel?.text = beer.beerName
-////        cell.detailTextLabel?.text = "\(beer.beerStyle) ABV: \(beer.beerAbv) | IBU: \(beer.beerIbu)"
-//
-//        return cell
-//    }
-//
+extension CellarViewController {
+
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cellarBeer.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.beerCell, for: indexPath) as! BeerTableViewCell
+        let beer = cellarBeer[indexPath.row]
+        cell.beerName.text = beer["beerName"] as? String
+        cell.breweryName.text = beer["breweryName"] as? String
+        cell.beerStyle.text = beer["beerStyle"] as? String
+        cell.beerLabel.sd_setImage(with: URL(string: (beer["beerLabel"] as? String)!), placeholderImage: UIImage(named: "placeholder.png"))
+//        cell.textLabel?.text = beer.beerName
+//        cell.detailTextLabel?.text = "\(beer.beerStyle) ABV: \(beer.beerAbv) | IBU: \(beer.beerIbu)"
+
+        return cell
+    }
+
 //    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 //        let destinationVC = segue.destination as! BeerShowViewController
 //
@@ -78,5 +94,5 @@ class CellarViewController: UITableViewController {
 //            destinationVC.beerToShow = cellarBeer[indexPath.row]
 //        }
 //    }
-//
-//}
+
+}
