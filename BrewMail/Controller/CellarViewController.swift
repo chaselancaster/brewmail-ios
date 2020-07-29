6b8e02225cee8a2ba591740a3a316206b8da17d3
chaselancaster <chaseblancaster@gmail.com>
2020-07-29 15:53:16 -0700

    Reload tableView on main thread after updating cellarBeer. Delete unnecessary code.

--- BrewMail/Controller/CellarViewController.swift
+++ BrewMail/Controller/CellarViewController.swift
@@ -40,41 +40,25 @@ class CellarViewController: UITableViewController {
         guard let uid = Auth.auth().currentUser?.uid else { return }
         
         let docRef = db.collection("users").document("\(uid)").collection("cellarBeer")
-
+        
+        // Grabbing user cellarBeer from Firestore
         docRef.getDocuments { (snapshot, error) in
-           
+            
             if let error = error {
                 print("Error retreiving documents from cellarBeer:", error)
             } else if let snapshot = snapshot {
                 let firestoreCellarBeer = snapshot.documents.compactMap() { (document) -> BeerModel? in
                     return try? document.data(as: BeerModel.self)
                 }
-//                print("firestoreCellarBeer -->", firestoreCellarBeer)
                 self.cellarBeer = firestoreCellarBeer
-                print("cellarBeer after firestoreCellarBeer -->", self.cellarBeer)
+                print("cellarBeer has been populated with the user's cellarBeer in Firestore")
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
             }
             
         }
-          
-        
-        
-        
-//        db.collection("users").document("\(uid)").collection("cellarBeer").getDocuments() { (querySnapshot, err) in
-//            if let err = err {
-//                print("Error getting documents: \(err)")
-//            } else {
-//                for document in querySnapshot!.documents {
-//                    print("\(document.documentID) => \(document.data())")
-//                    self.cellarBeer.append(document.data())
-//                }
-//                print("cellarBeer ---->", self.cellarBeer)
-//                // Reloading tableView to display cellar beer to user
-//                DispatchQueue.main.async {
-//                    self.tableView.reloadData()
-//                }
-//            }
-//        }
-        
+ 
     }
     
 }
@@ -82,38 +66,38 @@ class CellarViewController: UITableViewController {
 // MARK: - Table View Code
 
 extension CellarViewController {
-
+    
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 100
     }
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return cellarBeer.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: Constants.beerCell, for: indexPath) as! BeerTableViewCell
         let beer = cellarBeer[indexPath.row]
-        cell.beerName.text = beer["beerName"] as? String
-        cell.breweryName.text = beer["breweryName"] as? String
-        cell.beerStyle.text = beer["beerStyle"] as? String
-        cell.beerLabel.sd_setImage(with: URL(string: (beer["beerLabel"] as? String)!), placeholderImage: UIImage(named: "placeholder.png"))
-//        cell.textLabel?.text = beer.beerName
-//        cell.detailTextLabel?.text = "\(beer.beerStyle) ABV: \(beer.beerAbv) | IBU: \(beer.beerIbu)"
-
+        cell.beerName.text = beer.beerName
+        cell.breweryName.text = beer.breweryName
+        cell.beerStyle.text = beer.beerStyle
+        cell.beerLabel.sd_setImage(with: URL(string: beer.beerLabel), placeholderImage: UIImage(named: "placeholder.png"))
+        //        cell.textLabel?.text = beer.beerName
+        //        cell.detailTextLabel?.text = "\(beer.beerStyle) ABV: \(beer.beerAbv) | IBU: \(beer.beerIbu)"
+        
         return cell
     }
-
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        let destinationVC = segue.destination as! BeerShowViewController
-//
-//        if let indexPath = tableView.indexPathForSelectedRow {
-//            destinationVC.beerToShow = cellarBeer[indexPath.row]
-//        }
-//    }
-
+    
+    //    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    //        let destinationVC = segue.destination as! BeerShowViewController
+    //
+    //        if let indexPath = tableView.indexPathForSelectedRow {
+    //            destinationVC.beerToShow = cellarBeer[indexPath.row]
+    //        }
+    //    }
+    
 }
