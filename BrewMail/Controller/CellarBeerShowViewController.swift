e29f9d69d21d481c6af4040bc4350ba2fd2cc6ca
chaselancaster <chaseblancaster@gmail.com>
2020-07-30 10:30:14 -0700

    Add CellarBeerShowViewController file.

---
+++ BrewMail/Controller/CellarBeerShowViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CellarBeerShowViewController.swift
+//  BrewMail
+//
+//  Created by Chase Lancaster on 7/30/20.
+//  Copyright © 2020 Chase Lancaster. All rights reserved.
+//
+
+import Foundation
